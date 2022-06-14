9a083566ec672cbc9f24d3774f8f768ed2ca5cdc
Stefan Izota <1038308+stefanizota@users.noreply.github.com>
2022-06-14 14:18:58 +0300

    Added tdlibpath.

--- packages/telegram_client/lib/telegram_client.dart
+++ packages/telegram_client/lib/telegram_client.dart
@@ -1,4 +1,6 @@
+import 'dart:io' as io;
 import 'dart:ffi' as ffi;
+
 // import 'dart:convert' as convert;
 // import 'dart:ffi' as ffi;
 // import 'package:ffi/ffi.dart' as ffi_ext;
@@ -10,7 +12,18 @@ class TelegramClient {
   var _tdJsonClientId;
 
   void connect(int api_id, String api_hash, String phone) {
-    this._tdJsonClient = TdJsonClient(ffi.DynamicLibrary.open('libtdclient.a'));
+    this._tdJsonClient =
+        TdJsonClient(ffi.DynamicLibrary.open(this._getTdLibPath()));
     this._tdJsonClientId = this._tdJsonClient.td_create_client_id();
+  }
+
+  String _getTdLibPath() {
+    if (io.Platform.isMacOS) {
+      return 'libtdjson.dylib';
+    } else if (io.Platform.isWindows) {
+      return 'libtdjson.dylib';
+    } else {
+      return 'libtdjson.so';
+    }
   }
 }
