cd7ab74bdec2d2366e7cbee8ad912bc6770c88b4
Stefan Izota <1038308+stefanizota@users.noreply.github.com>
2022-06-14 15:32:08 +0300

    Implement telegram client connect (first draft).

--- packages/echo_cli/bin/main.dart
+++ packages/echo_cli/bin/main.dart
@@ -5,92 +5,9 @@ import 'package:telegram_client/telegram_client.dart';
 
 void main() {
   final telegramClient = TelegramClient();
+
   telegramClient.connect(telegram_config.getApiId(),
       telegram_config.getApiHash(), telegram_config.getPhoneNumber());
 
-//   final tdJsonLib = TdJsonClient(ffi.DynamicLibrary.open(_getPathTdJsonLib()));
-//   final tdJsonClientId = tdJsonLib.td_create_client_id();
-
-//   dynamic request = {'@type': 'getAuthorizationState', '@extra': 'some-hash'};
-//   tdJsonLib.td_send(tdJsonClientId,
-//       convert.jsonEncode(request).toNativeUtf8().cast<ffi.Char>());
-
-//   bool connected = false;
-//   bool done = false;
-
-//   const waitTimeout = 10.0;
-
-//   while (true) {
-//     ffi.Pointer<ffi.Int> tdResponse = tdJsonLib.td_receive(waitTimeout);
-
-//     if (tdResponse != ffi.nullptr) {
-//       var response =
-//           convert.jsonDecode(tdResponse.cast<ffi_ext.Utf8>().toDartString());
-
-//       if (response['@type'] == 'updateAuthorizationState') {
-//         var authorization_state = response['authorization_state'];
-
-//         if (authorization_state['@type'] == 'authorizationStateClosed') {
-//           break;
-//         }
-
-//         if (authorization_state['@type'] ==
-//             'authorizationStateWaitTdlibParameters') {
-//           request = {
-//             '@type': 'setTdlibParameters',
-//             'parameters': {
-//               'api_id': _getApiId(),
-//               'api_hash': _getApiHash(),
-//               'system_language_code': 'en',
-//               'use_message_database': false,
-//               'device_model': 'Desktop',
-//               'application_version': '1.0',
-//             }
-//           };
-//           tdJsonLib.td_send(tdJsonClientId,
-//               convert.jsonEncode(request).toNativeUtf8().cast<ffi.Char>());
-//         }
-
-//         if (authorization_state['@type'] ==
-//             'authorizationStateWaitTdlibParameters') {
-//           request = {
-//             '@type': 'checkDatabaseEncryptionKey',
-//             'encryption_key': '',
-//           };
-//           tdJsonLib.td_send(tdJsonClientId,
-//               convert.jsonEncode(request).toNativeUtf8().cast<ffi.Char>());
-//         }
-
-//         if (authorization_state['@type'] ==
-//             'authorizationStateWaitPhoneNumber') {
-//           request = {
-//             '@type': 'setAuthenticationPhoneNumber',
-//             'phone_number': _getPhoneNumber(),
-//           };
-//           tdJsonLib.td_send(tdJsonClientId,
-//               convert.jsonEncode(request).toNativeUtf8().cast<ffi.Char>());
-//         }
-
-//         if (authorization_state['@type'] == 'authorizationStateReady') {
-//           connected = true;
-//         }
-//       } else if (response['@type'] == 'updateNewMessage') {
-//         print('New Message\n');
-//         print(response);
-//       } else {
-//         print(response);
-//       }
-//     }
-
-//     if (connected == true) {
-//       if (done == false) {
-//         request = {
-//           '@type': 'getChats',
-//         };
-//         tdJsonLib.td_send(tdJsonClientId,
-//             convert.jsonEncode(request).toNativeUtf8().cast<ffi.Char>());
-//         done = true;
-//       }
-//     }
-//   }
+  telegramClient.disconnect();
 }
--- packages/telegram_client/lib/telegram_client.dart
+++ packages/telegram_client/lib/telegram_client.dart
@@ -1,7 +1,7 @@
 import 'dart:io' as io;
 import 'dart:ffi' as ffi;
-// import 'package:ffi/ffi.dart' as ffi_ext;
-// import 'dart:convert' as convert;
+import 'package:ffi/ffi.dart' as ffi_ext;
+import 'dart:convert' as convert;
 
 import 'package:telegram_client/src/lib_td_json.dart';
 
@@ -9,13 +9,80 @@ class TelegramClient {
   late final LibTdJson _libTdJson;
   late final ffi.Pointer<ffi.Int> _tdJsonClient;
 
-  void connect(int api_id, String api_hash, String phone) {
+  void connect(int apiId, String apiHash, String phoneNumber) {
     this._libTdJson = LibTdJson(ffi.DynamicLibrary.open(this._getTdLibPath()));
     this._tdJsonClient = this._libTdJson.td_json_client_create();
+
+    dynamic request;
+    const double waitTimeout = 10.0;
+
+    while (true) {
+      ffi.Pointer<ffi.Int> tdResponse = this._libTdJson.td_json_client_receive(
+          this._tdJsonClient.cast<ffi.Void>(), waitTimeout);
+
+      if (tdResponse != ffi.nullptr) {
+        var response =
+            convert.jsonDecode(tdResponse.cast<ffi_ext.Utf8>().toDartString());
+
+        if (response['@type'] == 'updateAuthorizationState') {
+          var authorization_state = response['authorization_state'];
+
+          if (authorization_state['@type'] == 'authorizationStateClosed') {
+            break;
+          }
+
+          if (authorization_state['@type'] ==
+              'authorizationStateWaitTdlibParameters') {
+            request = {
+              '@type': 'setTdlibParameters',
+              'parameters': {
+                'api_id': apiId,
+                'api_hash': apiHash,
+                'system_language_code': 'en',
+                'use_message_database': false,
+                'device_model': 'Desktop',
+                'application_version': '1.0',
+              }
+            };
+            this._libTdJson.td_json_client_send(
+                this._tdJsonClient.cast<ffi.Void>(),
+                convert.jsonEncode(request).toNativeUtf8().cast<ffi.Char>());
+          }
+
+          if (authorization_state['@type'] ==
+              'authorizationStateWaitTdlibParameters') {
+            request = {
+              '@type': 'checkDatabaseEncryptionKey',
+              'encryption_key': '',
+            };
+            this._libTdJson.td_json_client_send(
+                this._tdJsonClient.cast<ffi.Void>(),
+                convert.jsonEncode(request).toNativeUtf8().cast<ffi.Char>());
+          }
+
+          if (authorization_state['@type'] ==
+              'authorizationStateWaitPhoneNumber') {
+            request = {
+              '@type': 'setAuthenticationPhoneNumber',
+              'phone_number': phoneNumber,
+            };
+            this._libTdJson.td_json_client_send(
+                this._tdJsonClient.cast<ffi.Void>(),
+                convert.jsonEncode(request).toNativeUtf8().cast<ffi.Char>());
+          }
+
+          if (authorization_state['@type'] == 'authorizationStateReady') {
+            break;
+          }
+        }
+      }
+    }
   }
 
-  void disconnect() {
-    this._libTdJson.td_json_client_destroy(this._tdJsonClient.cast<ffi.Void>());
+  int disconnect() {
+    return this
+        ._libTdJson
+        .td_json_client_destroy(this._tdJsonClient.cast<ffi.Void>());
   }
 
   String _getTdLibPath() {
