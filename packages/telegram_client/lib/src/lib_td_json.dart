98c314dbd4f1f5bab6bdad48b3d63dbf2ef3fbd5
Stefan Izota <1038308+stefanizota@users.noreply.github.com>
2022-06-14 14:40:09 +0300

    Renamed libtdjson. Added disconnect.

--- packages/telegram_client/lib/src/lib_td_json.dart
+++ packages/telegram_client/lib/src/lib_td_json.dart
@@ -4,17 +4,17 @@
 import 'dart:ffi' as ffi;
 
 /// Generate binding to tdlib JSON
-class TdJsonClient {
+class LibTdJson {
   /// Holds the symbol lookup function.
   final ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName)
       _lookup;
 
   /// The symbols are looked up in [dynamicLibrary].
-  TdJsonClient(ffi.DynamicLibrary dynamicLibrary)
+  LibTdJson(ffi.DynamicLibrary dynamicLibrary)
       : _lookup = dynamicLibrary.lookup;
 
   /// The symbols are looked up with [lookup].
-  TdJsonClient.fromLookup(
+  LibTdJson.fromLookup(
       ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName)
           lookup)
       : _lookup = lookup;
--- packages/telegram_client/lib/telegram_client.dart
+++ packages/telegram_client/lib/telegram_client.dart
@@ -1,20 +1,21 @@
 import 'dart:io' as io;
 import 'dart:ffi' as ffi;
-
-// import 'dart:convert' as convert;
-// import 'dart:ffi' as ffi;
 // import 'package:ffi/ffi.dart' as ffi_ext;
+// import 'dart:convert' as convert;
 
-import 'package:telegram_client/src/td_json_client.dart';
+import 'package:telegram_client/src/lib_td_json.dart';
 
 class TelegramClient {
-  var _tdJsonClient;
-  var _tdJsonClientId;
+  late final LibTdJson _libTdJson;
+  late final ffi.Pointer<ffi.Int> _tdJsonClient;
 
   void connect(int api_id, String api_hash, String phone) {
-    this._tdJsonClient =
-        TdJsonClient(ffi.DynamicLibrary.open(this._getTdLibPath()));
-    this._tdJsonClientId = this._tdJsonClient.td_create_client_id();
+    this._libTdJson = LibTdJson(ffi.DynamicLibrary.open(this._getTdLibPath()));
+    this._tdJsonClient = this._libTdJson.td_json_client_create();
+  }
+
+  void disconnect() {
+    this._libTdJson.td_json_client_destroy(this._tdJsonClient.cast<ffi.Void>());
   }
 
   String _getTdLibPath() {
