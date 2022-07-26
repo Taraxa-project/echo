a113019fb8152adbb58076c591b54fb2daa936d1
Stefan Izota <1038308+stefanizota@users.noreply.github.com>
2022-07-26 17:32:35 +0300

    added new messages.

--- packages/cli/lib/command/dartpad.dart
+++ packages/cli/lib/command/dartpad.dart
@@ -59,9 +59,20 @@ class TelegramCommandDartpad extends TelegramCommand {
 
     await Future.delayed(const Duration(seconds: 5));
     telegramClient.removeEventListener(login);
+    login.exit();
+
+    var newMessages = NewMesssages();
+    telegramClient.addeventListener(newMessages);
+    var newMessagesIsolated = await NewMesssages.isolate();
+    telegramClient.addeventListener(newMessagesIsolated);
+
+    await Future.delayed(const Duration(seconds: 20));
+    telegramClient.removeEventListener(newMessages);
+    telegramClient.removeEventListener(newMessagesIsolated);
+    newMessages.exit();
+    newMessagesIsolated.exit();
 
     await Future.delayed(const Duration(seconds: 5));
-    login.exit();
     telegramClient.exit();
   }
 }
@@ -74,7 +85,7 @@ abstract class TelegramEventGenerator {
 }
 
 abstract class TelegramEventListener {
-  late final String uniqueKey;
+  late String uniqueKey;
   void update(event);
 
   void exit() {}
@@ -215,12 +226,12 @@ class TelegramClientIsolated extends TelegramClient {
 
   @override
   void removeEventListener(TelegramEventListener telegramEventListener) {
-    super.removeEventListener(telegramEventListener);
-
     _isolateSendPort?.send(RemoveEventListener(SendPortEventListener(
       telegramEventListener.uniqueKey,
       _isolateReceivePort.sendPort,
     )));
+
+    super.removeEventListener(telegramEventListener);
   }
 
   Future<void> spawn() async {
@@ -444,6 +455,7 @@ class LoginIsolated extends Login {
       required super.checkAuthenticationPasswordWithCallback}) {
     _isolateReceivePort = ReceivePort();
     _isolateReceivePortBroadcast = _isolateReceivePort.asBroadcastStream();
+    uniqueKey = Uuid().v1();
   }
 
   @override
@@ -454,10 +466,6 @@ class LoginIsolated extends Login {
   @override
   void auth() {
     _isolateSendPort?.send(Auth());
-  }
-
-  void setTelegramClient(TelegramSend telegramClient) {
-    _telegramClient = telegramClient;
   }
 
   Future<void> spawn() async {
@@ -560,4 +568,87 @@ class CheckAuthenticationPasswordWithCallback
   CheckAuthenticationPasswordWithCallback({
     required this.readUserPassword,
   });
+}
+
+class NewMesssages extends TelegramEventListener {
+  NewMesssages() {
+    uniqueKey = Uuid().v1();
+  }
+
+  @override
+  void update(event) {
+    // print('[${Isolate.current.debugName}._onUpdateNewMessage] ${event}');
+
+    if (event is UpdateNewMessage) {
+      _onUpdateNewMessage(event);
+    } else if (event is Error) {
+      _onError(event);
+    }
+  }
+
+  void _onUpdateNewMessage(UpdateNewMessage updateNewMessage) {
+    var content = updateNewMessage.message?.content;
+    if (content is MessageText && content.text is FormattedText) {
+      print(
+          '[${Isolate.current.debugName}._onUpdateNewMessage] ${content.text?.text}');
+    }
+  }
+
+  void _onError(Error error) {
+    print(error);
+  }
+
+  void exit() {}
+
+  static Future<NewMesssagesIsolated> isolate() async {
+    NewMesssagesIsolated newMesssagesIsolated = NewMesssagesIsolated();
+    await newMesssagesIsolated.spawn();
+    return newMesssagesIsolated;
+  }
+}
+
+class NewMesssagesIsolated extends NewMesssages {
+  late final ReceivePort _isolateReceivePort;
+  late final Stream<dynamic> _isolateReceivePortBroadcast;
+  SendPort? _isolateSendPort;
+
+  NewMesssagesIsolated() {
+    _isolateReceivePort = ReceivePort();
+    _isolateReceivePortBroadcast = _isolateReceivePort.asBroadcastStream();
+    uniqueKey = Uuid().v1();
+  }
+
+  @override
+  void update(event) {
+    _isolateSendPort?.send(Update(event));
+  }
+
+  Future<void> spawn() async {
+    await Isolate.spawn(
+      NewMesssagesIsolated._entryPoint,
+      [NewMesssages(), _isolateReceivePort.sendPort],
+      debugName: runtimeType.toString(),
+    );
+
+    _isolateSendPort = await _isolateReceivePortBroadcast.first;
+  }
+
+  static void _entryPoint(List<dynamic> initialSpawnMessage) {
+    NewMesssages newMesssages = initialSpawnMessage[0];
+    SendPort parentSendPort = initialSpawnMessage[1];
+
+    var receivePort = ReceivePort();
+    parentSendPort.send(receivePort.sendPort);
+
+    receivePort.listen((message) {
+      if (message is Update) {
+        newMesssages.update(message.event);
+      }
+    });
+  }
+
+  @override
+  void exit() {
+    _isolateReceivePort.close();
+  }
 }
