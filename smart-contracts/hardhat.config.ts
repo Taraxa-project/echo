2360388165ab35402c6c6e4e78de2ed9d006560b
Guilherme Barreiro Vieira <guibvieira@gmail.com>
2023-03-24 16:24:58 +0000

    add hardhat config to increase testing timeouts and enable configurable number of accounts

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -3,11 +3,26 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.14",
+  mocha: {
+    timeout: 5000000
+  },
   gasReporter: {
     enabled:true,
     // outputFile: "gas-report.txt",
     // noColors: false
-  }
+  },
+  networks: {
+    hardhat: {
+        accounts: {
+          mnemonic: "test test test test test test test test test test test junk",
+          path: "m/44'/60'/0'/0",
+          initialIndex: 0,
+          count: 100,
+          passphrase: "",
+        },
+      },
+    }
+  
 };
 
 export default config;
