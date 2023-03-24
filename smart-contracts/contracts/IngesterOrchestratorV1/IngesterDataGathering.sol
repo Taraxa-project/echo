e294244438594212adbba35261f021995b906503
Guilherme Barreiro Vieira <guibvieira@gmail.com>
2023-03-24 16:43:41 +0000

    add IPFS data gathering contract function documentation

--- smart-contracts/contracts/IngesterOrchestratorV1/IngesterDataGathering.sol
+++ smart-contracts/contracts/IngesterOrchestratorV1/IngesterDataGathering.sol
@@ -5,9 +5,23 @@ pragma solidity 0.8.14;
 import "../interfaces/IngesterOrchestratorV1/IIngesterDataGathering.sol";
 import "./IngesterRegistryAccessControl.sol";
 
+/**
+ * @title IngesterDataGathering
+ * @dev This contract manages the storage of IPFS hashes stored by registered ingesters.
+ * It extends the IngesterRegistryAccessControl which manages access control and ownership for the Ingester Registry and IngesterDataGathering interface
+ */
 contract IngesterDataGathering is  IngesterRegistryAccessControl, IIngesterDataGathering {
+
     mapping(address => IIngesterDataGathering.IpfsHash) public _ipfsHashes;
 
+    /**
+     * @notice Adds IPFS hashes for a registered ingester.
+     * @dev Can only be called by a registered ingester.
+     * @param ingesterAddress The address of the registered ingester.
+     * @param usersHash The IPFS hash of the users data.
+     * @param chatsHash The IPFS hash of the chats data.
+     * @param messagesHash The IPFS hash of the messages data.
+     */
     function addIpfsHash(address ingesterAddress, string memory usersHash, string memory chatsHash, string memory messagesHash ) external onlyRegistered {
         // make struct with all the different type of hashes
         IIngesterDataGathering.IpfsHash memory ipfsHashUsers = IIngesterDataGathering.IpfsHash(usersHash, chatsHash, messagesHash);
@@ -15,6 +29,12 @@ contract IngesterDataGathering is  IngesterRegistryAccessControl, IIngesterDataG
         emit IIngesterDataGathering.IpfsHashAdded(ingesterAddress, usersHash, chatsHash, messagesHash);
     }
 
+    /**
+     * @notice Gets the IPFS hashes for a registered ingester.
+     * @dev Can be called by anyone.
+     * @param ingesterAddress The address of the registered ingester.
+     * @return The IPFS hashes of the ingester's data.
+     */
     function getIpfsHashes(address ingesterAddress) public view returns(IpfsHash memory ipfsHashes) { // IpfsHash which interface would this belong to 
         return _ipfsHashes[ingesterAddress];
     }
--- smart-contracts/contracts/IngesterOrchestratorV2/IngesterDataGatheringV2.sol
+++ smart-contracts/contracts/IngesterOrchestratorV2/IngesterDataGatheringV2.sol
@@ -4,12 +4,25 @@ pragma solidity 0.8.14;
 import "../interfaces/IngesterOrchestratorV2/IIngesterDataGatheringV2.sol";
 import "./IngesterRegistryAccessControlV2.sol";
 
+/**
+ * @title IngesterDataGatheringV2
+ * @dev This contract manages the storage of IPFS hashes stored by registered ingesters.
+ * It extends the IngesterRegistryAccessControlV2 which manages access control and ownership for the Ingester Registry and IngesterDataGatheringV2 interface
+ */
 contract IngesterDataGatheringV2 is
     IngesterRegistryAccessControlV2,
     IIngesterDataGatheringV2
 {
     mapping(address => IIngesterDataGatheringV2.IpfsHash) public _ipfsHashes;
 
+    /**
+     * @notice Adds IPFS hashes for a registered ingester.
+     * @dev Can only be called by a registered ingester.
+     * @param ingesterAddress The address of the registered ingester.
+     * @param usersHash The IPFS hash of the users data.
+     * @param chatsHash The IPFS hash of the chats data.
+     * @param messagesHash The IPFS hash of the messages data.
+     */
     function addIpfsHash(
         address ingesterAddress,
         string memory usersHash,
@@ -32,6 +45,12 @@ contract IngesterDataGatheringV2 is
         );
     }
 
+    /**
+     * @notice Gets the IPFS hashes for a registered ingester.
+     * @dev Can be called by anyone.
+     * @param ingesterAddress The address of the registered ingester.
+     * @return The IPFS hashes of the ingester's data.
+     */
     function getIpfsHashes(
         address ingesterAddress
     ) public view returns (IpfsHash memory ipfsHashes) {
