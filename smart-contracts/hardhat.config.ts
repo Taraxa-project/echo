a5f59f19ba307a6cb7bad28476d7e32b22259d2f
Guilherme Barreiro Vieira <guibvieira@gmail.com>
2023-05-02 16:14:36 +0100

    add network configurations and solidstate to package.json

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -12,34 +12,42 @@ const config: HardhatUserConfig = {
     }
   },
   mocha: {
-    timeout: 5000000
+    timeout: 10000000
   },
   gasReporter: {
     enabled:true
   },
   networks: {
-    // taraxa_testnet: {
+    mainnet: {
+      url: 'https://rpc.mainnet.taraxa.io/',
+      accounts: [''],
+      chainId: 841,
+      gas: 10000000, 
+      gasPrice: 1, 
+    },
+    // testnet: {
     //   url: 'https://rpc.testnet.taraxa.io/',
     //   accounts: [''],
     //   chainId: 842, // Replace with Taraxa testnet's chainId
     //   gas: 10000000, // Custom gas limit; adjust this value as needed
-    //   gasPrice: 1000000000, // Custom gas price in gwei; adjust this value as needed (1 gwei in this example)
+    //   gasPrice: 1, // Custom gas price in gwei; adjust this value as needed (1 gwei in this example)
     // },
-    // taraxa_devnet: {
+    // devnet: {
     //   url: 'https://rpc.devnet.taraxa.io/',
     //   accounts: [''],
     //   chainId: 843, // Replace with Taraxa testnet's chainId
     //   gas: 100000000000, // Custom gas limit; adjust this value as needed
-    //   gasPrice: 1000000000, // Custom gas price in gwei; adjust this value as needed (1 gwei in this example)
+    //   gasPrice: 1, // Custom gas price in gwei; adjust this value as needed (1 gwei in this example)
     // },
     hardhat: {
         accounts: {
           mnemonic: "test test test test test test test test test test test junk",
           path: "m/44'/60'/0'/0",
           initialIndex: 0,
-          count: 100,
+          count: 500,
           passphrase: "",
         },
+        
       },
     }
 };
