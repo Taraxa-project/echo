b98a0fe4d9a8e2cfba917137c02b8f2bd60cbe99
Guilherme Barreiro Vieira <guibvieira@gmail.com>
2023-05-24 19:03:10 +0100

    update solidity compiler version to 0.8.18 across the project

--- smart-contracts/contracts/test/facets/AccessControlFacetTest.sol
+++ smart-contracts/contracts/test/facets/AccessControlFacetTest.sol
@@ -1,6 +1,6 @@
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
 import "@solidstate/contracts/access/access_control/AccessControl.sol";
 import "../../libraries/LibDiamond.sol";
--- smart-contracts/contracts/test/facets/GroupManagerFacetTest.sol
+++ smart-contracts/contracts/test/facets/GroupManagerFacetTest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
 import { LibAppStorageTest, AppStorageTest } from  "../libraries/LibAppStorageUpgradeTest.sol";
 import "../../interfaces/IIngesterGroupManager.sol";
