798aa77533377282dad9ce733cacfb92083a5fd0
stefanizota <1038308+stefanizota@users.noreply.github.com>
2023-08-29 16:18:46 +0300

    Develop (#138)
    * libtdjonlc 1.8.15
    
    * continue on exception
    
    * More scripts.

--- client/packages/cli/tool/scripts/smart_contract/add_groups.dart
+++ client/packages/cli/tool/scripts/smart_contract/add_groups.dart
@@ -9,6 +9,8 @@ import 'package:telegram_client/src/smart_contract/GroupManagerFacet.g.dart';
 void main() async {
   final credentials =
       EthPrivateKey.fromHex(String.fromEnvironment('private_key'));
+  // print(credentials.address);
+  // io.exit(0);
 
   final web3client = Web3Client(String.fromEnvironment('rpc_url'), Client());
   final contract = GroupManagerFacet(
@@ -20,24 +22,28 @@ void main() async {
   final file = new io.File(String.fromEnvironment('file_telegram_groups'));
   final groups = file.readAsLinesSync();
 
+  var nonce = 1;
   for (var groupIndex = 0; groupIndex < groups.length; groupIndex++) {
+    nonce = nonce + 1;
+    final transaction = Transaction(maxGas: 25000000, nonce: nonce);
     final groupName = groups[groupIndex].trim();
     var result;
     try {
       result = await contract.addGroup(
         groupName,
         credentials: credentials,
-        transaction: Transaction(maxGas: 25000000),
+        transaction: transaction,
       );
     } on RPCError catch (ex) {
       result = ex.toString();
     }
 
-    await Future.delayed(const Duration(seconds: 20));
+    await Future.delayed(const Duration(milliseconds: 20));
 
     var groupCountRemote = (await contract.getGroupCount()).toInt();
     print({
       'localIndex': groupIndex,
+      'nonce': nonce,
       'groupName': groupName,
       'groupCountRemote': groupCountRemote,
       'addGroupResult': result,
--- client/packages/cli/tool/scripts/smart_contract/unregister_ingester.dart
+++ client/packages/cli/tool/scripts/smart_contract/unregister_ingester.dart
@@ -1,9 +1,6 @@
-import 'dart:math';
-
 import 'package:web3dart/json_rpc.dart';
 import 'package:http/http.dart';
 import 'package:web3dart/web3dart.dart';
-import 'package:uuid/uuid.dart';
 
 import 'package:telegram_client/src/smart_contract/RegistryFacet.g.dart';
 
@@ -20,26 +17,10 @@ void main() async {
     client: web3client,
   );
 
-  final nonce = BigInt.from(Random().nextInt(100));
-  final message = Uuid().v4();
-
-  var messageHash = await contract.hash(
-    credentialsIngester.address,
-    message,
-    nonce,
-  );
-  var sig = credentialsOwner.signPersonalMessageToUint8List(messageHash);
-
   var result;
   try {
-    result = await contract.registerIngester(
-      credentialsIngester.address,
-      message,
-      nonce,
-      sig,
-      credentials: credentialsOwner,
-      transaction: Transaction(maxGas: 25000000),
-    );
+    result = await contract.unRegisterIngester(credentialsIngester.address,
+        credentials: credentialsOwner);
   } on RPCError catch (ex) {
     result = ex.toString();
   }
